e3e8decbc898c29b3611d4c28a030ed286720b1c
Juan Cruz Favaro <juaanfavaro@gmail.com>
2022-10-13 12:08:10 -0300

    Seteo ruta para enviar cookie

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const app = express()
 //Inicializo antes de las rutas porque es un middleware
 app.use(cookieParser())
 
+app.get("/", (req,res) => {
+    res.send("Hello World")
+})
+
+app.get("/setCookie", (req,res) => {
+    res.cookie('MyCookie', 'firstCookie', {
+        maxAge: 10000,
+        // expires: new Date("2022-10-13"),
+        httpOnly: true, //Para que no se puedan ver desde el navegador con document.cookie
+        secure: true, //Podemos acceder a la cookie solo cuando usemos HTTPS
+        sameSite: 'lax', //Si tenemos el front y backend en distintos dominios podemos habilitar a que se lean o no
+    })
+    res.send("Cookie sent")
+})
+
 
 
 
